c27784f34ca607a270013be58614536469e38a64
Đăng Tây <nguyendang01092003@gmail.com>
2025-05-13 23:05:07 +0700

    Up

--- Best1Mall-Front-End/wwwroot/modules/constants.js
+++ Best1Mall-Front-End/wwwroot/modules/constants.js
@@ -179,7 +179,7 @@ var HTML_CONSTANTS = {
         </div>`,
         GlobalSearchByKeyword: ` 
         <li class="p-2 hover:bg-red-50 hover:text-red-500 cursor-pointer flex items-center gap-2 text-xs">
-            <img src="assets/images/product1.jpg" alt="Sản phẩm" class="w-14 h-14 object-cover rounded">
+            <img src="/assets/images/product1.jpg" alt="Sản phẩm" class="w-14 h-14 object-cover rounded">
             <div>
                 <a href="{url}">{name}</a>
                 <div class="text-red-400 mt-1">185.000 đ</div>
