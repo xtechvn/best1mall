6854be609f91fa99c4a1e2483f221ecd8d0c7f0c
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-05-15 09:16:07 +0700

    update register by google

--- Best1Mall-Front-End/Controllers/Account/AuthController.cs
+++ Best1Mall-Front-End/Controllers/Account/AuthController.cs
@@ -98,7 +98,89 @@ public class AuthController : Controller
             return StatusCode(500, "Đã xảy ra lỗi trong quá trình đăng nhập bằng Google.");
         }
     }
+    [HttpGet("api/Auth/GoogleRegisterCallback")]
+    public async Task<IActionResult> GoogleRegisterCallback(string code)
+    {
+        if (string.IsNullOrEmpty(code))
+        {
+            return BadRequest("Mã ủy quyền (code) không hợp lệ.");
+        }
 
+        try
+        {
+            string clientId = _configuration["Authentication:Google:ClientId"];
+            string clientSecret = _configuration["Authentication:Google:ClientSecret"];
+            string redirectUri = _configuration["Authentication:Google:RedirectUriRegister"];
+            HttpContext.Response.Headers.Add("Cross-Origin-Opener-Policy", "same-origin");
+            ViewBag.Data = "";
+            var flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = clientId,
+                    ClientSecret = clientSecret
+                },
+                Scopes = new[] { "openid", "profile", "email" } // Các scope bạn đã yêu cầu
+            });
+            string domain = Request.Host.Host;
+            string scheme = Request.Scheme; // Lấy giao thức (http hoặc https)
+            string host = Request.Host.Value; // Lấy tên miền và cổng (ví dụ: localhost:2335)
+            string fullDomain = $"{scheme}://{domain}{redirectUri}";
+            var tokenResponse = await flow.ExchangeCodeForTokenAsync("me", code, fullDomain, CancellationToken.None);
+
+            if (string.IsNullOrEmpty(tokenResponse.IdToken))
+            {
+                return BadRequest("Không nhận được ID Token từ Google.");
+            }
+
+            var settings = new GoogleJsonWebSignature.ValidationSettings
+            {
+                Audience = new[] { clientId }
+            };
+
+            var payload = await GoogleJsonWebSignature.ValidateAsync(tokenResponse.IdToken, settings);
+
+            if (payload != null)
+            {
+                string email = payload.Email;
+                string googleId = payload.Subject;
+
+                var request = new ClientRegisterRequestModel()
+                {
+                    user_name = email,
+                    password = googleId,
+                    token = googleId,
+                    confirm_password = googleId,
+                    email = email,
+                    is_receive_email = true,
+                    phone=""
+                };
+
+                var result = await _clientServices.Register(request);
+                if(result!=null && result.data != null)
+                {
+                    ViewBag.Data = Newtonsoft.Json.JsonConvert.SerializeObject(result.data);
+
+                }
+                
+                return View();
+            }
+            else
+            {
+                return BadRequest("ID Token Google không hợp lệ.");
+            }
+        }
+        catch (InvalidJwtException ex)
+        {
+            return BadRequest($"ID Token Google không hợp lệ: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            LogHelper.InsertLogTelegramByUrl(_configuration["BotSetting:bot_token"], _configuration["BotSetting:bot_group_id"], "GoogleRegisterCallback - Authentication:" + ex.ToString());
+            // Log lỗi
+            return StatusCode(500, "Đã xảy ra lỗi trong quá trình đăng nhập bằng Google.");
+        }
+    }
     private string GenerateAppToken(object userData)
     {
         // Implement logic tạo token ứng dụng
--- Best1Mall-Front-End/wwwroot/modules/account_google.js
+++ Best1Mall-Front-End/wwwroot/modules/account_google.js
@@ -1,31 +1,66 @@
-﻿$(document).ready(function () {
-    var googleClientId = '231824741925-dr41odfkd6jm8l2oochnt0efvn842t4e.apps.googleusercontent.com'; // Thay YOUR_GOOGLE_CLIENT_ID bằng Client ID của bạn
-    var redirectUri = window.location.origin+ '/api/Auth/GoogleSignInCallback'
+﻿var googleClientId = '231824741925-dr41odfkd6jm8l2oochnt0efvn842t4e.apps.googleusercontent.com'; 
 
-    $('.btn-login-gg').on('click', function () {
-        // Xác định URL ủy quyền của Google
-        const googleAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth?client_id=' + googleClientId + '&redirect_uri=' + redirectUri + '&response_type=code&scope=openid%20profile%20email&prompt=select_account';
+$(document).ready(function () {
 
-        // Mở popup
-        const popupWindow = window.open(googleAuthUrl, 'google_login', 'width=600,height=500');
-        // Theo dõi cửa sổ popup và xử lý khi nó đóng
-        const popupInterval = setInterval(() => {
-            if (popupWindow && popupWindow.closed) {
-                clearInterval(popupInterval);
-                // Kiểm tra xem dữ liệu đã được truyền từ popup chưa
-                var token_local = localStorage.getItem(STORAGE_NAME.Login);
-                if (token_local == null || token_local == undefined || token_local.trim() == '' || token_local.trim() == 'null'
-                    || token_local.trim() == 'undefined') {
-                    $(':input[type="submit"]').prop('disabled', false);
-
-                    $('#dangnhap .user input').closest('.form-group').find('.err').show()
-                    $('#dangnhap .user input').closest('.form-group').find('.err').html(NOTIFICATION_MESSAGE.LoginIncorrect)
-                } else {
-                   
-                    window.location.reload();
-
-                }
-            }
-        }, 500);
-    });
+    account_google.DynamicBind()
+    
 });
+var account_google = {
+    DynamicBind: function () {
+
+        $('.btn-login-gg').on('click', function () {
+            var redirectUri = window.location.origin + '/api/Auth/GoogleSignInCallback'
+            // Xác định URL ủy quyền của Google
+            const googleAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth?client_id=' + googleClientId + '&redirect_uri=' + redirectUri + '&response_type=code&scope=openid%20profile%20email&prompt=select_account';
+
+            // Mở popup
+            const popupWindow = window.open(googleAuthUrl, 'google_login', 'width=600,height=500');
+            // Theo dõi cửa sổ popup và xử lý khi nó đóng
+            const popupInterval = setInterval(() => {
+                if (popupWindow && popupWindow.closed) {
+                    clearInterval(popupInterval);
+                    // Kiểm tra xem dữ liệu đã được truyền từ popup chưa
+                    var token_local = localStorage.getItem(STORAGE_NAME.Login);
+                    if (token_local == null || token_local == undefined || token_local.trim() == '' || token_local.trim() == 'null'
+                        || token_local.trim() == 'undefined') {
+                        $(':input[type="submit"]').prop('disabled', false);
+
+                        $('#dangnhap .user input').closest('.form-group').find('.err').show()
+                        $('#dangnhap .user input').closest('.form-group').find('.err').html(NOTIFICATION_MESSAGE.LoginIncorrect)
+                    } else {
+
+                        window.location.reload();
+
+                    }
+                }
+            }, 500);
+        });
+        $('.btn-register-gg').on('click', function () {
+            var redirectUri = window.location.origin + '/api/Auth/GoogleRegisterCallback'
+            // Xác định URL ủy quyền của Google
+            const googleAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth?client_id=' + googleClientId + '&redirect_uri=' + redirectUri + '&response_type=code&scope=openid%20profile%20email&prompt=select_account';
+
+            // Mở popup
+            const popupWindow = window.open(googleAuthUrl, 'google_login', 'width=600,height=500');
+            // Theo dõi cửa sổ popup và xử lý khi nó đóng
+            const popupInterval = setInterval(() => {
+                if (popupWindow && popupWindow.closed) {
+                    clearInterval(popupInterval);
+                    // Kiểm tra xem dữ liệu đã được truyền từ popup chưa
+                    var token_local = localStorage.getItem(STORAGE_NAME.Login);
+                    if (token_local == null || token_local == undefined || token_local.trim() == '' || token_local.trim() == 'null'
+                        || token_local.trim() == 'undefined') {
+                        $(':input[type="submit"]').prop('disabled', false);
+
+                        $('#dangnhap .user input').closest('.form-group').find('.err').show()
+                        $('#dangnhap .user input').closest('.form-group').find('.err').html(NOTIFICATION_MESSAGE.LoginIncorrect)
+                    } else {
+
+                        window.location.reload();
+
+                    }
+                }
+            }, 500);
+        });
+    }
+}
