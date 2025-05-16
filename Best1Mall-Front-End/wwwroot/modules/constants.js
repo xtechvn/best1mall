b99f172e9b549e31ad0dee7e321c54cff07a59d0
Hiếu <anhhieuk50@gmail.com>
2025-05-16 13:11:36 +0700

    update js

--- Best1Mall-Front-End/wwwroot/modules/constants.js
+++ Best1Mall-Front-End/wwwroot/modules/constants.js
@@ -149,7 +149,7 @@ var HTML_CONSTANTS = {
         GroupProductItem: ` 
       <div class="flex-shrink-0 w-27 text-center p-2 rounded-xl border border-blue-100">
                         <div class="rounded-xl bg-blue-50 mb-2 p-2 h-22 w-22 flex items-center justify-center">
-                            <a href="{url}">
+                            <a href="/san-pham?group_id={id}">
                                 <img src="{avt}" alt="{name}" class="mx-auto h-15">
                             </a>
                         </div>
