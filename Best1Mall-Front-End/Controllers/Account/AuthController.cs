5f90fba0352215c44ec4b6e80ada1578b813f714
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-05-18 17:25:32 +0700

    Update AuthController.cs

--- Best1Mall-Front-End/Controllers/Account/AuthController.cs
+++ Best1Mall-Front-End/Controllers/Account/AuthController.cs
@@ -44,7 +44,7 @@ public class AuthController : Controller
             string clientId = _configuration["Authentication:Google:ClientId"];
             string clientSecret = _configuration["Authentication:Google:ClientSecret"];
             string redirectUri = _configuration["Authentication:Google:RedirectUri"];
-            HttpContext.Response.Headers.Add("Cross-Origin-Opener-Policy", "same-origin");
+           // HttpContext.Response.Headers.Add("Cross-Origin-Opener-Policy", "same-origin");
 
             var flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
             {
@@ -120,7 +120,7 @@ public class AuthController : Controller
             string clientId = _configuration["Authentication:Google:ClientId"];
             string clientSecret = _configuration["Authentication:Google:ClientSecret"];
             string redirectUri = _configuration["Authentication:Google:RedirectUriRegister"];
-            HttpContext.Response.Headers.Add("Cross-Origin-Opener-Policy", "same-origin");
+            //HttpContext.Response.Headers.Add("Cross-Origin-Opener-Policy", "same-origin");
             ViewBag.Data = "";
             var flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
             {
