27d9af5fda8fc199b6eab00b7bb21139104b7a13
Đăng Tây <nguyendang01092003@gmail.com>
2025-06-05 18:12:36 +0700

    updateLabel

--- Best1Mall-Front-End/Models/Products/ProductListResponseModel.cs
+++ Best1Mall-Front-End/Models/Products/ProductListResponseModel.cs
@@ -6,5 +6,14 @@ namespace Best1Mall_Front_End.Models.Products
     {
         public List<ProductMongoDbModel> items { get; set; }
         public long count { get; set; }
+        public LabelDetail? label_detail { get; set; }
+    }
+    public class LabelDetail
+    {
+        public int id { get; set; }
+        public string labelName { get; set; }
+        public string labelCode { get; set; }
+        public string icon { get; set; }
+        public string banner { get; set; }
     }
 }
