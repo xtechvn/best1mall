5d45dee93ca0e3ea09dc2f1fad5d6aa51123983f
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-06-27 11:26:50 +0700

    update

--- Best1Mall-Front-End/Models/Orders/OrdersUpdateAddressRequestModel.cs
+++ Best1Mall-Front-End/Models/Orders/OrdersUpdateAddressRequestModel.cs
@@ -5,11 +5,17 @@
         public string province_id { get; set; }
         public string district_id { get; set; }
         public string ward_id { get; set; }
+
         public string token { get; set; }
-        public string address { get; set; }
         public string phone { get; set; }
+
+        public string address { get; set; }
+        public string id { get; set; }
         public long order_id { get; set; }
 
+        public long address_id { get; set; }
+
+        public string receiver_name { get; set; }
 
     }
 }
