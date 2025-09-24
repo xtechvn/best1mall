85405877e8d595d16eb5177916b72f4c12f8afeb
xtechvn <nguyendang01092003@gmail.com>
2025-09-24 11:07:35 +0700

    up

--- Best1Mall-Front-End/utilities/Helper/BadgeHelper.cs
+++ Best1Mall-Front-End/utilities/Helper/BadgeHelper.cs
@@ -9,9 +9,15 @@
             switch (badgeType.Value)
             {
                 case 117: return "/assets/images/tag-sieusale.png"; // SupperSale
+
+                case 243: return "/assets/images/tag-sieusale.png"; // SupperSale(new)
+
                 case 130: return "/assets/images/tag-banchay.png"; // Sale
                 case 114: return "/assets/images/tag-hottrend.png"; // Bestchoice
+                case 287: return "/assets/images/tag-hottrend.png"; // Bestchoice(new)
                 case 113: return "/assets/images/tag-noibat.png"; // Featuredproduct
+                case 112: return "/assets/images/tag-noibat.png"; // Featuredproduct(new)
+
                 default: return ""; // Không hợp lệ => không render ảnh
             }
         }
